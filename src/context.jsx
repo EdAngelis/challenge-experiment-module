fc6947b903b0f40bfbe95db9f8a16572d7c4bd51
Ed Angelis <ed4ngelis@gmail.com>
2024-03-01 16:02:48 -0300

    experiment model template

--- src/context.jsx
+++ src/context.jsx
@@ -4,7 +4,39 @@ export const AppContext = createContext();
 
 export function AppProvider({ children }) {
 
-    const [experiments, setExperiments] = useState([{}])
+    const [experiments, setExperiments] = useState([{
+      id: 1,
+      blocked: false,
+      open: false,
+      iterations: [{
+        id: 1,
+        title: 'Iteration 1',
+        prompt: '',
+        selected: true,
+        open: false,
+      },] 
+    },
+    {
+      id: 2,
+      blocked: true,
+      open: false,
+      iterations: [{
+        id: 1,
+        title: 'Iteration 1',
+        prompt: '',
+        selected: true,
+        open: false,
+      },
+      {
+        id: 2,
+        title: 'Iteration 2',
+        prompt: '',
+        selected: true,
+        open: false,
+      }
+    ] 
+    }])
+    
     return (
         <AppContext.Provider value = {{ experiments, setExperiments}}>
             {children}
