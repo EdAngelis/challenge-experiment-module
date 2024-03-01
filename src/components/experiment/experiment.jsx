72434a5a2f3d859479d3fa58039c00134abe3c14
Ed Angelis <ed4ngelis@gmail.com>
2024-03-01 14:47:30 -0300

    directories starter

---
+++ src/components/experiment/experiment.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './experiment.module.css'
+
+export default function Experiment() {
+  return (
+    <div>Experiment</div>
+  )
+}
---
+++ src/components/iteration/iteration.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from './iteration.module.css'
+export default function Iteration() {
+  return (
+    <div>Iteration</div>
+  )
+}
