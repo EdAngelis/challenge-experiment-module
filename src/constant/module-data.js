777f86f97468302a9c2315c6b6658a31c105ec2d
Ed Angelis <ed4ngelis@gmail.com>
2024-03-02 10:05:11 -0300

    refactor: constant placed outside context file

---
+++ src/constant/module-data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    blocked: false,
+    open: false,
+    iterations: [
+      {
+        id: 1,
+        title: 'Iteration 1',
+        size: '',
+        selected: true,
+        open: false
+      }
+    ]
+  },
+  {
+    id: 2,
+    blocked: true,
+    open: false,
+    iterations: [
+      {
+        id: 2,
+        title: 'Iteration 1',
+        size: '',
+        selected: false,
+        open: false
+      },
+      {
+        id: 3,
+        title: 'Iteration 2',
+        size: '',
+        selected: true,
+        open: false
+      }
+    ]
+  }
+]
--- src/context.jsx
+++ src/context.jsx
@@ -1,45 +1,9 @@
 import { createContext, useState } from 'react'
-
+import data from './constant/module-data.js'
 export const AppContext = createContext()
 
 export function AppProvider ({ children }) {
-  const [experiments, setExperiments] = useState([
-    {
-      id: 1,
-      blocked: false,
-      open: false,
-      iterations: [
-        {
-          id: 1,
-          title: 'Iteration 1',
-          size: '',
-          selected: true,
-          open: false
-        }
-      ]
-    },
-    {
-      id: 2,
-      blocked: true,
-      open: false,
-      iterations: [
-        {
-          id: 2,
-          title: 'Iteration 1',
-          size: '',
-          selected: false,
-          open: false
-        },
-        {
-          id: 3,
-          title: 'Iteration 2',
-          size: '',
-          selected: true,
-          open: false
-        }
-      ]
-    }
-  ])
+  const [experiments, setExperiments] = useState(data)
 
   const toggleOpen = (experiment) => {
     const newExperiments = experiments.map((ex) => {
