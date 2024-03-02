78cc6c4cb70997cb24ca1893aa295ea108977bc5
Ed Angelis <ed4ngelis@gmail.com>
2024-03-02 20:00:49 -0300

    refactor: set up unique id

--- src/context.jsx
+++ src/context.jsx
@@ -1,115 +1,121 @@
-import { createContext, useState } from 'react'
-import data from './constant/module-data.js'
-export const AppContext = createContext()
+import { createContext, useState } from "react";
+import data from "./constant/module-data.js";
+export const AppContext = createContext();
 
-export function AppProvider ({ children }) {
-  const [experiments, setExperiments] = useState(data)
+export function AppProvider({ children }) {
+  const [experiments, setExperiments] = useState(data);
 
   const addModule = () => {
+    const uniqueId = () => parseInt(Date.now() * Math.random()).toString();
     setExperiments([
       ...experiments,
       {
-        id: experiments.length + 1,
+        id: uniqueId,
         blocked: false,
         open: false,
-        iterations: []
-      }
-    ])
-  }
+        iterations: [],
+      },
+    ]);
+  };
 
   const removeModule = (id) => {
-    const newExperiments = experiments.filter((ex) => ex.id !== id)
-    setExperiments(newExperiments)
-  }
+    const newExperiments = experiments.filter((ex) => ex.id !== id);
+    setExperiments(newExperiments);
+  };
 
   const toggleOpen = (experiment) => {
     const newExperiments = experiments.map((ex) => {
       if (ex.id === experiment.id) {
-        ex.open = !ex.open
+        ex.open = !ex.open;
       }
-      return ex
-    })
-    setExperiments(newExperiments)
-  }
+      return ex;
+    });
+    setExperiments(newExperiments);
+  };
 
   const blockUnblockExperiment = (experiment) => {
     const newExperiments = experiments.map((ex) => {
       if (ex.id === experiment.id) {
-        ex.blocked = !ex.blocked
+        ex.blocked = !ex.blocked;
       }
-      return ex
-    })
-    setExperiments(newExperiments)
-  }
+      return ex;
+    });
+    setExperiments(newExperiments);
+  };
 
   const resetExperiment = (experiment) => {
     const newExperiments = experiments.map((ex) => {
       if (ex.id === experiment.id) {
-        ex.iterations = []
+        ex.iterations = [];
       }
-      return ex
-    })
-    setExperiments(newExperiments)
-  }
+      return ex;
+    });
+    setExperiments(newExperiments);
+  };
 
   const addIteration = (id, newIteration) => {
+    const uniqueId = () => parseInt(Date.now() * Math.random()).toString();
     const newExperiments = experiments.map((ex) => {
       if (ex.id === id) {
-        ex.iterations.push({ title: newIteration, selected: true })
+        ex.iterations.push({
+          id: uniqueId,
+          title: newIteration,
+          selected: true,
+        });
       }
-      return ex
-    })
-    setExperiments(newExperiments)
-  }
+      return ex;
+    });
+    setExperiments(newExperiments);
+  };
 
   const toggleIterationOpen = (open, id) => {
     const newExperiments = experiments.map((ex) => {
       ex.iterations = ex.iterations.map((it) => {
         if (it.id === id) {
-          it.open = open
+          it.open = open;
         }
-        return it
-      })
-      return ex
-    })
+        return it;
+      });
+      return ex;
+    });
 
-    setExperiments(newExperiments)
-  }
+    setExperiments(newExperiments);
+  };
 
   const setSize = (size, id) => {
     const newExperiments = experiments.map((ex) => {
       ex.iterations = ex.iterations.map((it) => {
         if (it.id === id) {
-          it.size = size
+          it.size = size;
         }
-        return it
-      })
-      return ex
-    })
-    setExperiments(newExperiments)
-  }
+        return it;
+      });
+      return ex;
+    });
+    setExperiments(newExperiments);
+  };
 
   const removeIteration = (id) => {
     const newExperiments = experiments.map((ex) => {
-      ex.iterations = ex.iterations.filter((it) => it.id !== id)
-      return ex
-    })
+      ex.iterations = ex.iterations.filter((it) => it.id !== id);
+      return ex;
+    });
 
-    setExperiments(newExperiments)
-  }
+    setExperiments(newExperiments);
+  };
 
   const toggleSelected = (id) => {
     const newExperiments = experiments.map((ex) => {
       ex.iterations = ex.iterations.map((it) => {
         if (it.id === id) {
-          it.selected = !it.selected
+          it.selected = !it.selected;
         }
-        return it
-      })
-      return ex
-    })
-    setExperiments(newExperiments)
-  }
+        return it;
+      });
+      return ex;
+    });
+    setExperiments(newExperiments);
+  };
 
   return (
     <AppContext.Provider
@@ -125,10 +131,10 @@ export function AppProvider ({ children }) {
         removeIteration,
         toggleSelected,
         addModule,
-        removeModule
+        removeModule,
       }}
     >
       {children}
     </AppContext.Provider>
-  )
+  );
 }
